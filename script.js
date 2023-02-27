c71fcc12cfaecbfec1854eee87f71e384628972c
Des <93647728+AetherDeS@users.noreply.github.com>
2023-02-27 19:39:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function (){	
+// 	document.getElementById("burger-btn").addEventListener("click", function (){
+// 		document.getElementById("burger-btn").classList.toggle("active");
+// 		document.querySelector(".menu").classList.toggle("active");
+// 	});
+// 	document.getElementById("burger-btn").addEventListener("hover", () => {
+// 		document.getElementById("burger-element").classList.add("background-orange");
+// 	});
+// })
