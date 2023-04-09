ee22399a6b1eb7a22a40ba61e43a33e1f1540375
Des <93647728+AetherDeS@users.noreply.github.com>
2023-04-09 23:56:52 +0300

    Delete Core directory

--- Core/script.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function (){	
-// 	document.getElementById("burger-btn").addEventListener("click", function (){
-// 		document.getElementById("burger-btn").classList.toggle("active");
-// 		document.querySelector(".menu").classList.toggle("active");
-// 	});
-// 	document.getElementById("burger-btn").addEventListener("hover", () => {
-// 		document.getElementById("burger-element").classList.add("background-orange");
-// 	});
-// })
